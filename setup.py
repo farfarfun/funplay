061a2206ed0053404523287b48226efc0213a1a4
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-07-15 14:31:52 +0800

    add

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ from notetool.tool import get_version
 
 version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
 
-version = get_version(sys.argv, version_path, step=16)
+version = get_version(sys.argv, version_path, step=32)
 
-install_requires = []
-
+install_requires = ['notedrive']
 setup(name='noteplay',
       version=version,
       description='noteplay',
@@ -19,5 +18,5 @@ setup(name='noteplay',
       url='https://github.com/1007530194',
 
       packages=find_packages(),
-      install_requires=install_requires
+      install_requires=install_requires,
       )
