9ea29ed344bcecc6a87d7482e712f6068c7de8f9
farfarfun <farfarfun@qq.com>
2026-08-30 10:14:29 +0800

    test: add minimal smoke test (tests/test_smoke.py)
    Refs farfarfun/todo-list#304

---
+++ tests/test_smoke.py
@@ -0,0 +1,10 @@
+import funplay
+import funplay.tianchi
+
+
+def test_import_funplay():
+    assert funplay is not None
+
+
+def test_import_funplay_tianchi():
+    assert funplay.tianchi is not None
